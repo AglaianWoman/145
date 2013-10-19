a818e5b3dcfe6cd11cfb9dbd015368e4a103594d
lauramiron <laurabmiron@gmail.com>
2013-10-19 03:26:33 -0700

    sql files working for items

--- create.sql
+++ create.sql
@@ -1,1 +1,4 @@
-#Create tables and loads data into sql
+create table Item (ItemID int PRIMARY KEY, Name string, Currently string, Buy_price string, First_bid string, Started string, Ends string, UserID string, Description string);
+create table User (UserID string, Rating string, Location string, Country string);
+create table Category (ItemID int, Category string);
+create table Bid (ItemID int, UserID string, time string, amount string);
